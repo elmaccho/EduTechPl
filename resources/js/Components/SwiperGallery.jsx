114ffa47e37605c6936b90bdb13389c845d2fd87
elmaccho <106253529+elmaccho@users.noreply.github.com>
2024-06-02 16:16:05 +0200

    Swiper gallery - more slides

--- resources/js/Components/SwiperGallery.jsx
+++ resources/js/Components/SwiperGallery.jsx
@@ -8,6 +8,8 @@ import "/resources/css/swiper.css";
 import { Pagination, Navigation } from "swiper/modules";
 
 import slide1 from "/public/img/swiper gallery/pexels-tirachard-kumtanom-112571-733856 1.png";
+import slide2 from "/public/img/swiper gallery/pexels-george-dolgikh-551816-1310532.jpg";
+import slide3 from "/public/img/swiper gallery/pexels-divinetechygirl-1181233.jpg";
 
 export default function SwiperGallery({}) {
     return (
@@ -23,8 +25,8 @@ export default function SwiperGallery({}) {
             className="mySwiper mb-5"
         >
             <SwiperSlide>
-                <img src={slide1} alt="" />
-                <div className="slide1-info">
+                <img src={slide1} alt="slide1" />
+                <div className="slide-info slide1-info">
                     <p className="h4">
                         <b>
                             Zaufało nam już ponad 200 000 studentów z całego
@@ -35,8 +37,31 @@ export default function SwiperGallery({}) {
                     <p>Z kodem <b>tryMe</b> 30% taniej na pierwszy kurs</p>
                 </div>
             </SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>
+                <img src={slide2} alt="slide2" />
+                <div className="slide-info slide2-info">
+                    <p className="h4">
+                        <b>
+                            Ucz się w dowolnym miejscu i czasie!
+                        </b>
+                    </p>
+                    <p>Dostęp do materiałów 24/7 na komputerze, tablecie lub smartfonie.</p>
+                    <p>Indywidualne podejście i wsparcie na każdym etapie nauki.</p>
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide3} alt="slide3" />
+                <div className="slide-info slide3-info">
+                    <p className="h4">
+                        <b>
+                            Zaufało nam już ponad 200 000 studentów z całego
+                            świata.
+                        </b>
+                    </p>
+                    <p>Zacznij naukę już dziś!</p>
+                    <p>Z kodem <b>tryMe</b> 30% taniej na pierwszy kurs</p>
+                </div>
+            </SwiperSlide>
         </Swiper>
     );
 }
